2fbf2ec65771a10729e06b029c8534d41a44b0f5
danilo <danimoraga1997@gmail.com>
2025-11-19 19:46:32 +0100

    Create a description of the typography script

--- scripts/typographies.js
+++ scripts/typographies.js
@@ -1,1 +1,26 @@
 
+
+// This is a new typography script that takes into account the fact that
+// different sections of a page break at different points. 
+// A potential solution is described at a general level inside the main function
+
+function main() {
+    console.log("Running typography.js");
+
+    // Set event listeners for:
+    //  - Load page
+    //  - Change page width
+
+    // You should create an object that has all the elements that change their typography
+    // You should have:
+    //  - The typography for each version
+    //  - The width point at which they change their "typography"
+
+    // Every time an event listener is triggered, you should iterate over the list of elements
+    // and, given their specific breakpoints, apply the right typography
+
+    // You could improve this by only changing the typography when there is no previous typography
+    // or when the element's version changes
+}
+
+main();
